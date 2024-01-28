b041a85de0576ad627cfdd01c67106587645af7a
aalazmi <156626053+aalazmi@users.noreply.github.com>
2024-01-28 14:04:49 +0500

    instructions component made

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../../component/login/login.component';
 import { ThankyouComponent } from '../../component/thankyou/thankyou.component';
 import { SignupComponent } from '../../component/signup/signup.component';
+import { InstructionsComponent } from '../../component/instructions/instructions.component';
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
@@ -11,4 +12,8 @@ export const routes: Routes = [
     path: 'thankyou',
     component: ThankyouComponent,
   },
+  {
+    path: "instructions",
+    component : InstructionsComponent
+  }
 ];
